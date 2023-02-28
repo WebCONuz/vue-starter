ce319c67270984faa0d5fbfb5505f22b34966d14
WebCONuz <muxammadi0799@gmail.com>
2023-02-28 09:40:26 +0500

    Vue Starter

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template lang="">
+  <main>
+    <RouterView />
+  </main>
+</template>
+<script>
+import { RouterView } from "vue-router";
+
+export default {
+  name: "App",
+};
+</script>
+<style lang=""></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template lang="">
+  <div class="container">
+    <h1 class="text-3xl text-red-600 font-bold py-6">HOME PAGE</h1>
+    <p class="text-lg text-justify">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque minus
+      reiciendis dignissimos eius iusto nostrum. Ullam iure esse exercitationem
+      cupiditate beatae nam excepturi, explicabo rem labore dolor eveniet
+      ducimus dignissimos laudantium! Iure commodi consectetur, sequi error
+      obcaecati soluta voluptate fugit! Distinctio explicabo nam molestias
+      soluta libero, saepe ipsam iure, accusantium impedit incidunt, optio error
+      repellat maiores laborum odio facere quos commodi perferendis. Odit,
+      laborum quis. Molestias architecto a vero quidem aspernatur veniam officia
+      sunt alias quibusdam excepturi tenetur autem totam quos qui minus sapiente
+      error reiciendis, ullam consequuntur? Maiores amet harum vitae consectetur
+      magnam consequuntur cumque exercitationem omnis sequi libero.
+    </p>
+  </div>
+</template>
+<script>
+export default {
+  name: "Home",
+};
+</script>
+<style lang=""></style>
